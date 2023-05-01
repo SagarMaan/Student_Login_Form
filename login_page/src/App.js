a831c78097d1eee51b8b96349c5e1b48ff2ca9aa
Sagar <sagarmaan2015@gmail.com>
2023-05-01 09:37:00 +0530

    APP file

--- login_page/src/App.js
+++ login_page/src/App.js
@@ -1,9 +1,24 @@
+import { createBrowserRouter , RouterProvider } from "react-router-dom"
+import Form from "./component/Form/Form";
+import TableData from "./component/Table/Table";
 
-export default function App() {
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Form/>
+  },
+  {
+    path : "/table",
+    element : <TableData/>
+  }
+])
+
+function App() {
   return (
-    <>
-    hello World
-    </>   
-  )  
+      <main>
+        <RouterProvider router={router} ></RouterProvider>
+      </main>
+  );
 }
 
+export default App;
